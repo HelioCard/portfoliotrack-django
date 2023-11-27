33c954ac38a27f4a4c14765c87911edd3adea5b3
Hélio Cardoso <helio.card@yahoo.com.br>
2023-11-27 12:37:52 -0300

    change in url of transactions

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('upload_file/', views.upload_file, name="upload_file"),
     path('download_model_file/', views.download_model_file, name="download_model_file"),
     path('register_transaction/', views.register_transaction, name="register_transaction"),
-    path('transactions/', views.transactions, name='transactions'),
+    path('', views.transactions, name='transactions'),
     path('delete_transaction/', views.delete_transaction, name='delete_transaction'),
     path('edit_transaction/<int:pk>/', views.edit_transaction, name='edit_transaction'),
 ]
