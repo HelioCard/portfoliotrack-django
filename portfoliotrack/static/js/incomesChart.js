7dbbdab58ae328549c879a6c4fe5afd5ece83c4c
Hélio Cardoso <helio.card@yahoo.com.br>
2023-12-14 13:28:52 -0300

    minor improvements

--- portfoliotrack/static/js/incomesChart.js
+++ portfoliotrack/static/js/incomesChart.js
@@ -1,5 +1,6 @@
 const THEME = 'westeros';
 let incomesEvolutionChart
+let yieldEvolutionChart
 
 // Elementos do DOM
 const elements = {
@@ -7,10 +8,12 @@ const elements = {
     yieldOnCostText: document.querySelector('#yieldOnCostText'),
     averageDividendsText: document.querySelector('#averageDividendsText'),
     dividendEvolutionChartStatus: document.querySelector('#dividendEvolutionChartStatus'),
+    yieldEvolutionChartStatus:document.querySelector('#yieldEvolutionChartStatus'),
 }
 
 function showNoData() {
     elements.dividendEvolutionChartStatus.innerHTML = '<h6 class="display-6">Não há dados</h6>'
+    elements.yieldEvolutionChartStatus.innerHTML = '<h6 class="display-6">Não há dados</h6>'
 }
 
 // Obter dados do endpoint
@@ -42,10 +45,15 @@ async function updateIncomesEvolutionChart(URL) {
 
             incomesOptions.xAxis[0].data = incomes_evolution.date
             incomesOptions.series[0].data = incomes_evolution.dividends
-            incomesOptions.series[1].data = incomes_evolution.yield_on_cost
+
+            yieldOptions.xAxis[0].data = incomes_evolution.date
+            yieldOptions.series[0].data = incomes_evolution.yield_on_cost
 
             incomesEvolutionChart = echarts.init(document.querySelector('#dividendEvolutionChart'), THEME);
             incomesEvolutionChart.setOption(incomesOptions);
+
+            yieldEvolutionChart = echarts.init(document.querySelector('#yieldEvolutionChart'), THEME);
+            yieldEvolutionChart.setOption(yieldOptions);
 
             document.querySelector('#dividendsText').innerHTML = `R$ ${incomes_cards.total_dividends}`;
             document.querySelector('#yieldOnCostText').innerHTML = `${incomes_cards.total_yield_on_cost} %`;
@@ -54,15 +62,16 @@ async function updateIncomesEvolutionChart(URL) {
         }
     } catch (error) {
         console.error('Erro:', error);
-        alert('Ocorreu um erro ao atualizar o gráfico.');
+        alert('Ocorreu um erro ao atualizar os gráficos e mostrar os dados.');
     }
 };
 
 updateIncomesEvolutionChart(getIncomesEvolutiontURL)
 
 function resizeIncomesChart() {
-    if (incomesEvolutionChart) {
+    if (incomesEvolutionChart && yieldEvolutionChart) {
         incomesEvolutionChart.resize();
+        yieldEvolutionChart.resize();
     }
 };
 
--- portfoliotrack/static/js/incomesOptions.js
+++ portfoliotrack/static/js/incomesOptions.js
@@ -1,11 +1,17 @@
 
 var incomesOptions = {
-    legend: {
-      data: ['Dividendos', 'Yield on Cost']
+    title: {
+      subtext: 'Histórico dos dividendos ao longo do tempo',
+      left: 'center'
     },
     tooltip: {
-      trigger: 'axis',
-      
+      trigger: 'item',
+      formatter: function (params) {
+        const value = params.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        const date = params.name
+        const name = 'Dividendos:'
+        return name + '<br/>' + date + ': R$ ' + value
+      }
     },
     grid: {
       left: '3%',
@@ -28,7 +34,9 @@ var incomesOptions = {
         name: 'Dividendos (R$)',
         position: 'left',
         axisLabel: {
-          formatter: '{value}'
+          formatter: function (value) {
+            return value.toLocaleString('pt-BR', { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+          }
         },
         axisTick: {
           show: true,
@@ -37,20 +45,6 @@ var incomesOptions = {
           }
         },
       },
-      {
-        type: 'value',
-        name: 'Yield on Cost (%)',
-        position: 'right',
-        axisLabel: {
-          formatter: '{value}'
-        },
-        axisTick: {
-          show: true,
-          lineStyle: {
-            color: "#cccccc",
-          }
-        },
-      }
     ],
     series: [
       {
@@ -62,15 +56,69 @@ var incomesOptions = {
           data: [{ type: 'average', name: 'Média' }]
         }
       },
-      {
-        name: 'Yield on Cost',
-        type: 'line',
-        yAxisIndex: 1,
-        smooth: false,
-        data: [0.6, 0.9, 0.5, 0.4, 0.7, 0.6, 0.35],
-        markLine: {
-          data: [{ type: 'average', name: 'Média' }]
-        }
-      },
     ]
   };
+
+
+  
+var yieldOptions = {
+  title: {
+    subtext: 'Histórico do dividend yeld ao longo do tempo',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: function (params) {
+      const value = params.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+      const date = params.name
+      const name = 'Yield on Cost:'
+      return name + '<br/>' + date + ': ' + value + '%'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'Yield on Cost (%)',
+      position: 'left',
+      axisLabel: {
+        formatter: function (value) {
+          return value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        }
+      },
+      axisTick: {
+        show: true,
+        lineStyle: {
+          color: "#cccccc",
+        }
+      },
+    }
+  ],
+  series: [
+    {
+      name: 'Yield on Cost',
+      type: 'line',
+      yAxisIndex: 0,
+      smooth: false,
+      data: [0.6, 0.9, 0.5, 0.4, 0.7, 0.6, 0.35],
+      markLine: {
+        data: [{type: 'average', name: 'Média' }],
+      },
+      smooth: true,
+    },
+  ]
+};
